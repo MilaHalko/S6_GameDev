daf35fcfab67721cf8b582e42b5061de1f904826
Mila Halko <milagalko1@gmail.com>
2023-03-27 02:58:29 +0300

    add: Cameras left, right, start_level

--- Lab1/Assets/Scripts/Player/PlayerEntity.cs
+++ Lab1/Assets/Scripts/Player/PlayerEntity.cs
@@ -1,4 +1,5 @@
 using System;
+using Core.Enums;
 using UnityEngine;
 
 namespace Player
@@ -8,7 +9,7 @@ namespace Player
     {
         [Header("HorizontalMovement")]
         [SerializeField] private float _horizontalSpeed;
-        [SerializeField] private bool _faceRight;
+        [SerializeField] private Direction _direction;
 
         [Header("VerticalMovement")] 
         [SerializeField] private float _verticalSpeed;
@@ -23,6 +24,9 @@ namespace Player
         [SerializeField] private SpriteRenderer _shadow;
         [SerializeField] [Range(0, 1)] private float _shadowSizeModificator;
         [SerializeField] [Range(0, 1)] private float _shadowAlphaModificator;
+
+        [Header("Camera")] 
+        [SerializeField] private DirectionalCameraPair _cameras;
 
         private Rigidbody2D _rigidbody;
         
@@ -101,8 +105,8 @@ namespace Player
 
         private void SetDirection(float direction)
         {
-            if ((_faceRight && direction < 0) ||
-                (!_faceRight && direction > 0))
+            if ((_direction == Direction.Right && direction < 0) ||
+                (_direction == Direction.Left && direction > 0))
             {
                 Flip();
             }
@@ -111,7 +115,11 @@ namespace Player
         private void Flip()
         {
             transform.Rotate(0, 180, 0);
-            _faceRight = !_faceRight;
+            _direction = _direction == Direction.Right ? Direction.Left : Direction.Right;
+            foreach (var cameraPair in _cameras.DirectionalCameras)
+            {
+                cameraPair.Value.enabled = cameraPair.Key == _direction;
+            }
         }
 
         private void UpdateJump()
